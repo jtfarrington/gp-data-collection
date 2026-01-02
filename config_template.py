145e45fc16ddcc1717e62ac2073b6a8ecaa93cb7
Jacob F <j.t.farrington@outlook.com>
2026-01-02 16:49:47 -0600

    Add database config template for setup instructions

---
+++ config_template.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'your_mysql_username',      # Your MySQL username
+    'password': 'your_mysql_password',  # Your MySQL password
+    'database': 'match_data'
+}
